218a13746a5cb7a986ab0a43348b071aa5979735
Alexander Soloninov <etj.sad@gmail.com>
2020-03-08 03:33:32 +0100

    update
    update

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
-# PvP Shooter
-    Players versus players Action Shooter 
-    
-    Created by: inf3032 -> inf3032@hs-worms.de
-    at Hochschule Worms ( Germany )
+# Mobile-Shooter
+    Players versus players Mobile-Action Shooter
+
+    Created by: Alexander Soloninov
+    at Hochschule Worms in Germany
     for Mobile Visual Computing (SS 2018)
     ---------------------------------------
-    
-    Download demo under : /WindowsNoEditor
-      
-### Created in: 
+
+    Download demo under : /demo
+
+    Demo/Android
+    Demo/Windows_x64
+    Demo/Windows_UWP
+    Demo/Browser
+    Demo/Server
+
+### Created in:
     Visual Studio 2017
     Unreal Engine 4.19.1+
-   
+
 ### Platforms:
-    Windows 7 and later
-    Android 5.1 and later
-    IOS 8 and later ( not yet )
-    
+    Windows 8+ / 10
+    Android 5.1+
+    iOS 9+
+
 ### Supported Api
-    Directx 10.2 ( Only Windows )
-    Vulkan
-    
+    Directx 11 / 12.1
+    Vulkan Api Mobile 1.1
+
 ### LICENSE
     GNU General Public License v3.0
-
---
+++ Source/Shooter/ShooterGameSession.cpp
@@ -0,0 +1,36 @@
+
+#include "ShooterGameSession.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
+
+void AShooterGameSession::RegisterServer() {
+	Super::RegisterServer();
+
+	UE_LOG(LogTemp, Log, TEXT("Registering server and creating session --------"));
+	UE_LOG(LogTemp, Log, TEXT("Version 1.1.50"));
+	IOnlineSubsystem* OnlineSubSystem = IOnlineSubsystem::Get();
+	if (OnlineSubSystem == nullptr) {
+		UE_LOG(LogTemp, Error, TEXT("No online subsystem found!!"));
+		return;
+	}
+	UE_LOG(LogTemp, Log, TEXT("Got online subsystem"));
+	IOnlineSessionPtr Session = OnlineSubSystem->GetSessionInterface();
+	UE_LOG(LogTemp, Log, TEXT("Got session interface"));
+	FOnlineSessionSettings Settings;
+
+	Settings.bIsDedicated = true;
+	Settings.bIsLANMatch = false;
+	Settings.bAllowJoinInProgress = true;
+	Settings.bShouldAdvertise = true;
+	Settings.NumPublicConnections = 64;
+	Settings.bUsesPresence = true;
+
+	UE_LOG(LogTemp, Log, TEXT("Initialized settings"));
+
+	Session->CreateSession(0, GameSessionName, Settings);
+
+	UE_LOG(LogTemp, Log, TEXT("Session created --------")); 
+
+
+}
